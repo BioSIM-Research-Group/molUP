e721dbefdad46329ba462f97e4eff40130508948
Henrique Silva Fernandes <up201106591@fc.up.pt>
2016-11-17 15:31:39 +0000

    loadGaussianOutputFiles

--- gaussianVMD/lib/loadGaussianOutputFileFirstStructure.tcl
+++ gaussianVMD/lib/loadGaussianOutputFileFirstStructure.tcl
@@ -1,3 +1,9 @@
 package provide loadGaussianOutputFileFirstStructure 1.0
 
+### This procedure load a gaussian input file and converts it to PDB
 
+proc gaussianVMD::loadGaussianOutputFileFirstStructure {} {
+    
+    
+}
+    
