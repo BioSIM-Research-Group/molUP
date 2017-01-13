9c8605bc5a61f2686e1763bc6afcd6901be18d2a
Henrique Silva Fernandes <up201106591@fc.up.pt>
2017-01-13 14:00:43 +0000

    Tablelist 5.13 was included.

--- gaussianVMD/pkgIndex.tcl
+++ gaussianVMD/pkgIndex.tcl
@@ -38,3 +38,35 @@ package ifneeded saveFiles                                 	1.0 [list source [fi
 
 #Theme
 package ifneeded ttk::theme::clearlooks                     0.1 [list source [file join $dir/lib/theme/clearlooks.tcl]]
+
+
+
+#Tablelist
+#==============================================================================
+# Tablelist and Tablelist_tile package index file.
+#
+# Copyright (c) 2000-2015  Csaba Nemethi (E-mail: csaba.nemethi@t-online.de)
+#==============================================================================
+
+#
+# Regular packages:
+#
+package ifneeded tablelist         5.13 \
+	[list source [file join $dir/lib/tablelist tablelist.tcl]]
+package ifneeded tablelist_tile    5.13 \
+	[list source [file join $dir/lib/tablelist tablelist_tile.tcl]]
+
+#
+# Aliases:
+#
+package ifneeded Tablelist         5.13 \
+	[list package require -exact tablelist	    5.13]
+package ifneeded Tablelist_tile    5.13 \
+	[list package require -exact tablelist_tile 5.13]
+
+#
+# Code common to all packages:
+#
+package ifneeded tablelist::common 5.13 \
+        "namespace eval ::tablelist { proc DIR {} {return [list $dir]} } ;\
+	 source [list [file join $dir tablelistPublic.tcl]]"
