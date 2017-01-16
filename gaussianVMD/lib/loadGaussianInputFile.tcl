49b8747f276b35caf070485b3def8d92d2dfb137
Henrique Silva Fernandes <up201106591@fc.up.pt>
2017-01-16 16:51:16 +0000

    Open non-ONIOM Gaussian Input Files

--- gaussianVMD/lib/loadGaussianInputFile.tcl
+++ gaussianVMD/lib/loadGaussianInputFile.tcl
@@ -38,76 +38,149 @@ proc gaussianVMD::loadGaussianInputFile {} {
     #### Organize the structure info
     set allAtoms [split $gaussianVMD::structureGaussian \n]
 
-	#puts [llength [lindex $allAtoms 0]]
+	set numberColumns [llength [lindex $allAtoms 0]]
 
-	set i 0
-	foreach atom $allAtoms {
-			
-			lassign $atom column0 column1 column2 column3 column4 column5 column6 column7 column8
+	if {$numberColumns == 4} {
 
-	    	#### Condition to distinguish between ONIOM and simple calculations
+		#### Read an input file that has 4 columns
+
+		set i 0
+		foreach atom $allAtoms {
+			lassign $atom column0 column1 column2 column3
+
 			incr i
-			regexp {(\S+)[-](\S+)[-](\S+)[(]PDBName=(\S+),ResName=(\S+),ResNum=(\S+)[)]} $column0 -> \
-			atomicSymbol gaussianAtomType charge pdbAtomType resname resid
 
-			regexp {(\S+)\.+(\S+)} $column2 -> xbefore xafter
-	    	set x $xbefore\.[format %.3s $xafter]
-	    	regexp {(\S+)\.+(\S+)} $column3 -> ybefore yafter
-	    	set y $ybefore\.[format %.3s $yafter]
-	    	regexp {(\S+)\.+(\S+)} $column4 -> zbefore zafter
-	    	set z $zbefore\.[format %.3s $zafter]
-			
-			if {[string match "*--*" $column0]==1} {
-				set charge [expr $charge * -1] } else {
-			 }
-			
+			set resname "XXX"
+			set resid "1"
+			set pdbAtomType $column0
+			set column5 "A"
+			set atomicSymbol $column0
+
+			regexp {(\S+)\.+(\S+)} $column1 -> xbefore xafter
+		    set x $xbefore\.[format %.3s $xafter]
+		    regexp {(\S+)\.+(\S+)} $column2 -> ybefore yafter
+		    set y $ybefore\.[format %.3s $yafter]
+		    regexp {(\S+)\.+(\S+)} $column3 -> zbefore zafter
+		    set z $zbefore\.[format %.3s $zafter]
 
 			puts $gaussianVMD::temporaryPDBFile "[format %-4s "ATOM"] [format %6s $i] [format %-4s $pdbAtomType][format %4s $resname] [format %-1s $column5] [format %-7s $resid] [format %7s $x] [format %7s $y] [format %7s $z] [format %5s "1.00"] [format %-8s "00.00"] [format %8s $atomicSymbol]"
-			
+
 			$gaussianVMD::topGui.frame0.tabs.tabsAtomList.tab4.tableLayer insert end [list \
-	   			"$i" \
-	   			"[lindex [split $gaussianAtomType "-"] 0]" \
-	   			"$resname" \
-	   			"$resid" \
-	   			"$charge"\
-	   			]
-			   
-			$gaussianVMD::topGui.frame0.tabs.tabsAtomList.tab2.tableLayer insert end [list \
-	   			"$i" \
-	   			"$pdbAtomType" \
-	   			"$resname" \
-	   			"$resid" \
-	   			"$column5"\
-	   			]
-	   		
-			$gaussianVMD::topGui.frame0.tabs.tabsAtomList.tab3.tableLayer insert end [list \
-	   			"$i" \
-	   			"$pdbAtomType" \
-	   			"$resname" \
-	   			"$resid" \
-	   			"$column1"\
-	   			]
+		   			"$i" \
+		   			"$column0" \
+		   			"X" \
+		   			"0" \
+		   			""\
+		   			]
+				   
+				$gaussianVMD::topGui.frame0.tabs.tabsAtomList.tab2.tableLayer insert end [list \
+		   			"$i" \
+		   			"$column0" \
+		   			"X" \
+		   			"0" \
+		   			"L"\
+		   			]
+				   
+				$gaussianVMD::topGui.frame0.tabs.tabsAtomList.tab3.tableLayer insert end [list \
+		   			"$i" \
+		   			"$column0" \
+		   			"X" \
+		   			"0" \
+		   			"0"\
+		   			]
 
-			set atomicSymbol 		""
-			set gaussianAtomType 	"" 
-			set charge				""
-			set pdbAtomType			""
-			set resname				""
-			set resid				""
-			set x					""
-			set y					""
-			set z					""
-			set column0				""
-			set column1				""
-			set column2				""
-			set column3				""
-			set column4				""
-			set column5				""
-			set column6				""
-			set column7				""
-			set column8				""
+				set atomicSymbol 		""
+				set gaussianAtomType 	"" 
+				set charge				""
+				set pdbAtomType			""
+				set resname				""
+				set resid				""
+				set x					""
+				set y					""
+				set z					""
+				set column0				""
+				set column1				""
+				set column2				""
+				set column3				""
+		}
 
-	}	  
+	} elseif {$numberColumns > 4} {
+		
+		#### Read an input file that has 9 columns
+
+		set i 0
+		foreach atom $allAtoms {
+
+				lassign $atom column0 column1 column2 column3 column4 column5 column6 column7 column8
+
+		    	#### Condition to distinguish between ONIOM and simple calculations
+				incr i
+				regexp {(\S+)[-](\S+)[-](\S+)[(]PDBName=(\S+),ResName=(\S+),ResNum=(\S+)[)]} $column0 -> \
+				atomicSymbol gaussianAtomType charge pdbAtomType resname resid
+
+				regexp {(\S+)\.+(\S+)} $column2 -> xbefore xafter
+		    	set x $xbefore\.[format %.3s $xafter]
+		    	regexp {(\S+)\.+(\S+)} $column3 -> ybefore yafter
+		    	set y $ybefore\.[format %.3s $yafter]
+		    	regexp {(\S+)\.+(\S+)} $column4 -> zbefore zafter
+		    	set z $zbefore\.[format %.3s $zafter]
+
+				if {[string match "*--*" $column0]==1} {
+					set charge [expr $charge * -1] } else {
+				 }
+			 
+
+				puts $gaussianVMD::temporaryPDBFile "[format %-4s "ATOM"] [format %6s $i] [format %-4s $pdbAtomType][format %4s $resname] [format %-1s $column5] [format %-7s $resid] [format %7s $x] [format %7s $y] [format %7s $z] [format %5s "1.00"] [format %-8s "00.00"] [format %8s $atomicSymbol]"
+
+				$gaussianVMD::topGui.frame0.tabs.tabsAtomList.tab4.tableLayer insert end [list \
+		   			"$i" \
+		   			"[lindex [split $gaussianAtomType "-"] 0]" \
+		   			"$resname" \
+		   			"$resid" \
+		   			"$charge"\
+		   			]
+				   
+				$gaussianVMD::topGui.frame0.tabs.tabsAtomList.tab2.tableLayer insert end [list \
+		   			"$i" \
+		   			"$pdbAtomType" \
+		   			"$resname" \
+		   			"$resid" \
+		   			"$column5"\
+		   			]
+				   
+				$gaussianVMD::topGui.frame0.tabs.tabsAtomList.tab3.tableLayer insert end [list \
+		   			"$i" \
+		   			"$pdbAtomType" \
+		   			"$resname" \
+		   			"$resid" \
+		   			"$column1"\
+		   			]
+
+				set atomicSymbol 		""
+				set gaussianAtomType 	"" 
+				set charge				""
+				set pdbAtomType			""
+				set resname				""
+				set resid				""
+				set x					""
+				set y					""
+				set z					""
+				set column0				""
+				set column1				""
+				set column2				""
+				set column3				""
+				set column4				""
+				set column5				""
+				set column6				""
+				set column7				""
+				set column8				""
+
+		}
+
+	} else {
+		gaussianVMD::guiError "The file has a strange structure. The file cannot be openned."
+	}
+
 	
 	## Add a footer to the file
 	  puts $gaussianVMD::temporaryPDBFile "END"
