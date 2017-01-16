36e30d96ec2fd845265cd7f84ddde6568cb55475
Henrique Silva Fernandes <up201106591@fc.up.pt>
2017-01-16 16:59:31 +0000

    Open non-ONIOM output files- first strucutre

--- gaussianVMD/lib/loadGaussianOutputFile.tcl
+++ gaussianVMD/lib/loadGaussianOutputFile.tcl
@@ -5,8 +5,6 @@ proc gaussianVMD::loadGaussianOutputFile {option} {
     
     # option tells the procedure to get the first, the last, the optimizred or all structures
 
-	gaussianVMD::timeBegin
-
 	gaussianVMD::globalInfoOutputFile
 
 	#### Number of Atoms
@@ -36,6 +34,75 @@ proc gaussianVMD::loadGaussianOutputFile {option} {
 
         #### Organize the structure info
         set allAtoms [split $gaussianVMD::structureGaussian \n]
+
+		set numberColumns [llength [lindex $allAtoms 0]]
+
+	if {$numberColumns == 4} {
+
+		#### Read an input file that has 4 columns
+
+		set i 0
+		foreach atom $allAtoms {
+			lassign $atom column0 column1 column2 column3
+
+			incr i
+
+			set resname "XXX"
+			set resid "1"
+			set pdbAtomType $column0
+			set column5 "A"
+			set atomicSymbol $column0
+
+			regexp {(\S+)\.+(\S+)} $column1 -> xbefore xafter
+		    set x $xbefore\.[format %.3s $xafter]
+		    regexp {(\S+)\.+(\S+)} $column2 -> ybefore yafter
+		    set y $ybefore\.[format %.3s $yafter]
+		    regexp {(\S+)\.+(\S+)} $column3 -> zbefore zafter
+		    set z $zbefore\.[format %.3s $zafter]
+
+			puts $gaussianVMD::temporaryPDBFile "[format %-4s "ATOM"] [format %6s $i] [format %-4s $pdbAtomType][format %4s $resname] [format %-1s $column5] [format %-7s $resid] [format %7s $x] [format %7s $y] [format %7s $z] [format %5s "1.00"] [format %-8s "00.00"] [format %8s $atomicSymbol]"
+
+			$gaussianVMD::topGui.frame0.tabs.tabsAtomList.tab4.tableLayer insert end [list \
+		   			"$i" \
+		   			"$column0" \
+		   			"X" \
+		   			"0" \
+		   			""\
+		   			]
+				   
+				$gaussianVMD::topGui.frame0.tabs.tabsAtomList.tab2.tableLayer insert end [list \
+		   			"$i" \
+		   			"$column0" \
+		   			"X" \
+		   			"0" \
+		   			"L"\
+		   			]
+				   
+				$gaussianVMD::topGui.frame0.tabs.tabsAtomList.tab3.tableLayer insert end [list \
+		   			"$i" \
+		   			"$column0" \
+		   			"X" \
+		   			"0" \
+		   			"0"\
+		   			]
+
+				set atomicSymbol 		""
+				set gaussianAtomType 	"" 
+				set charge				""
+				set pdbAtomType			""
+				set resname				""
+				set resid				""
+				set x					""
+				set y					""
+				set z					""
+				set column0				""
+				set column1				""
+				set column2				""
+				set column3				""
+		}
+
+	} elseif {$numberColumns > 4} {
+
     	set i 0
     	foreach atom $allAtoms {
         
@@ -105,6 +172,10 @@ proc gaussianVMD::loadGaussianOutputFile {option} {
     			set column8				""
             
     	}	  
+		
+	} else {
+		gaussianVMD::guiError "The file has a strange structure. The file cannot be openned."
+	}
     
     	## Add a footer to the file
     	  puts $gaussianVMD::temporaryPDBFile "END"
@@ -114,12 +185,6 @@ proc gaussianVMD::loadGaussianOutputFile {option} {
 		
 		#### Load the molecule on VMD
 		gaussianVMD::loadMolecule $gaussianVMD::fileName $gaussianVMD::actualTime normal
-
-
-
-
-
-		gaussianVMD::timeEnd
 
 
     } elseif {$option == "lastStructure"} {
@@ -219,8 +284,6 @@ proc gaussianVMD::loadGaussianOutputFile {option} {
 		#### Load the molecule on VMD
 		gaussianVMD::loadMolecule $gaussianVMD::fileName $gaussianVMD::actualTime normal
 
-
-		gaussianVMD::timeEnd
         
     } elseif {$option == "optimizedStructures"} {
 
@@ -382,7 +445,6 @@ proc gaussianVMD::loadGaussianOutputFile {option} {
 			gaussianVMD::loadMolecule $gaussianVMD::fileName $gaussianVMD::actualTime normal
 		}
 
-		gaussianVMD::timeEnd
 
 
 		} else {
@@ -542,7 +604,6 @@ proc gaussianVMD::loadGaussianOutputFile {option} {
 		#### Load the molecule on VMD
 		gaussianVMD::loadMolecule $gaussianVMD::fileName $gaussianVMD::actualTime allStructures
 
-		gaussianVMD::timeEnd
 
 
     } else {
