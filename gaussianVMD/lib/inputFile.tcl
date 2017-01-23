e3641cb4015258a625f4a44f24c0b44aa3bda1c3
Henrique Silva Fernandes <up201106591@fc.up.pt>
2017-01-23 14:27:16 +0000

    Save pdb before load

--- gaussianVMD/lib/inputFile.tcl
+++ gaussianVMD/lib/inputFile.tcl
@@ -32,7 +32,7 @@ proc gaussianVMD::loadButton {fileExtension} {
 	#### Open a .com file
 	if {$gaussianVMD::fileExtension == ".com"} {
 		gaussianVMD::loadGaussianInputFile
-		#gaussianVMD::loadMolecule $gaussianVMD::fileName $gaussianVMD::actualTime normal
+		gaussianVMD::loadMolecule $gaussianVMD::fileName $gaussianVMD::actualTime normal
 		gaussianVMD::addSelectionRep
 
 	#### Open a .log file
