7cd7196c59d1cb1e94941e1f73402692ab06cae5
Henrique <up201106591@fc.up.pt>
2020-01-07 16:29:16 +0000

    v1.6.1

--- molUP/molUP.tcl
+++ molUP/molUP.tcl
@@ -52,7 +52,7 @@ namespace eval molUP:: {
 
 		#### Program Variables
 		## General
-		variable version	    	"1.6.0"
+		variable version	    	"1.6.1"
 
 		#GUI
         variable topGui         	".molUP"
